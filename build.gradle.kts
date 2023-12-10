6797be99af1eb38429e0f179296079ebd5f4571d
Бенедис Марк Валерьевич <mark.benedis@south.rt.ru>
2023-12-10 15:43:34 +0300

    add allure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+val allureVersion = "2.24.0"
+// Define the version of AspectJ
+val aspectJVersion = "1.9.20.1"
+
+// Define configuration for AspectJ agent
+val agent: Configuration by configurations.creating {
+    isCanBeConsumed = true
+    isCanBeResolved = true
+}
 
 plugins {
     kotlin("jvm") version "1.7.20"
@@ -15,10 +24,20 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.20")
+    // Import allure-bom to ensure correct versions of all the dependencies are used
+    testImplementation(platform("io.qameta.allure:allure-bom:$allureVersion"))
+    // Add necessary Allure dependencies to dependencies section
+    testImplementation("io.qameta.allure:allure-junit5")
+    // Add aspectjweaver dependency
+    agent("org.aspectj:aspectjweaver:${aspectJVersion}")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
 }
 
 tasks.test {
     useJUnitPlatform()
+    jvmArgs = listOf(
+        "-javaagent:${agent.singleFile}"
+    )
 }
 
 tasks.withType<KotlinCompile> {
